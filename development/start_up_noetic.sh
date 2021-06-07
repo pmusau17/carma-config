3865f12cd809fb03a4ddef543eb7e491215cbfdc
Patrick Musau <pmusau13ster@gmail.com>
2021-06-07 06:31:57 -0500

    removed local testing scripts

--- development/start_up_noetic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# start up background processes 
-docker-compose -f docker-compose-background.yml -p carma-background up -d
-
-# start up container
-docker-compose -f docker-compose.yml -p carma up   
